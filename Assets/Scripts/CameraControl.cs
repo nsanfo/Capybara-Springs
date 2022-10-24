e35993533328b9317d1d2659925d32ab30d25df4
Exojets <RyPK94@gmail.com>
2022-10-23 19:27:53 -0700

    Added edge scrolling and made CameraControls more readable

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,7 +9,7 @@ public class CameraControl : MonoBehaviour
     Plane ground;
     Vector3 intersection;
     public float cameraSpeed = 1.5f, maxZoom = 0.6f, minZoom = 5f;
-    float horizontalInput, forwardInput, scrollInput, mouseInput;
+    float horizontalInput, forwardInput, scrollInput, mouseInput, modifier;
 
     // Start is called before the first frame update
     void Start()
@@ -18,35 +18,66 @@ public class CameraControl : MonoBehaviour
 
     }
 
+    void KeyMove()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(((Vector3.right * Time.deltaTime * horizontalInput * cameraSpeed) / 0.6f) * modifier);
+        transform.Translate(((Vector3.forward * Time.deltaTime * forwardInput * cameraSpeed) / 0.6f) * modifier, tmp.transform);
+    }
+
+    void EdgeMove()
+    {
+        horizontalInput = (Input.mousePosition.x - (Screen.width / 2)) / (Screen.width / 2);
+        forwardInput = (Input.mousePosition.y - (Screen.height / 2)) / (Screen.height / 2);
+
+        transform.Translate(((Vector3.right * Time.deltaTime * horizontalInput * cameraSpeed) / 0.6f) * modifier);
+        transform.Translate(((Vector3.forward * Time.deltaTime * forwardInput * cameraSpeed) / 0.6f) * modifier, tmp.transform);
+    }
+
+    void Zoom()
+    {
+        scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        tmp.transform.rotation = transform.rotation;
+        tmp.transform.Translate(Vector3.forward * Time.deltaTime * scrollInput * 100);
+
+        if (tmp.transform.position.y >= maxZoom && tmp.transform.position.y <= minZoom)
+            transform.Translate(Vector3.forward * Time.deltaTime * scrollInput * 100);
+    }
+
+    void Rotate()
+    {
+        mouseInput = Input.GetAxis("Mouse X");
+        float enter = 0.0f;
+        ray = new Ray(transform.position, transform.forward);
+
+        if (ground.Raycast(ray, out enter))
+        {
+            intersection = ray.GetPoint(enter);
+            transform.RotateAround(intersection, Vector3.up, mouseInput * 12);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         tmp.transform.position = transform.position;
         tmp.transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        modifier = -0.1025f * Mathf.Pow((transform.position.y - 5), 2) + 2.583f;
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        mouseInput = Input.GetAxis("Mouse X");
+        KeyMove();
 
-        var modifier = -0.1025f * Mathf.Pow((transform.position.y - 5), 2) + 2.583f;
-        transform.Translate(((Vector3.right * Time.deltaTime * horizontalInput * cameraSpeed) / 0.6f) * modifier);
-        transform.Translate(((Vector3.forward * Time.deltaTime * forwardInput * cameraSpeed) / 0.6f) * modifier, tmp.transform);
+        if (Input.mousePosition.y >= Screen.height * 0.95 || Input.mousePosition.y <= Screen.height * 0.05 || Input.mousePosition.x >= Screen.width * 0.95 || Input.mousePosition.x <= Screen.width * 0.05)
+        {
+            EdgeMove();
+        }
 
-        tmp.transform.rotation = transform.rotation;
-        tmp.transform.Translate(Vector3.forward * Time.deltaTime * scrollInput * 100);
-        if (tmp.transform.position.y >= maxZoom && tmp.transform.position.y <= minZoom)
-            transform.Translate(Vector3.forward * Time.deltaTime * scrollInput * 100);
+        Zoom();
 
         if (Input.GetMouseButton(2))
         {
-            float enter = 0.0f;
-            ray = new Ray(transform.position, transform.forward);
-            if (ground.Raycast(ray, out enter))
-            {
-                intersection = ray.GetPoint(enter);
-            }
-            transform.RotateAround(intersection, Vector3.up, mouseInput * 12);
+            Rotate();
         }
     }
 }
