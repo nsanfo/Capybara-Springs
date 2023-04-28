619df0c67bdb1eb22a12773285701c237e8f4ed3
Raikuman <40256283+Raikuman@users.noreply.github.com>
2023-04-28 15:14:08 -0700

    Fix music crash

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,50 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
+
+class Song
+{
+    readonly private AudioSource song;
+    private bool played = false;
+
+    public Song(AudioSource song)
+    {
+        this.song = song;
+    }
+
+    public void PlaySong()
+    {
+        if (played) return;
+        played = true;
+        song.Play();
+    }
+
+    public bool IsPlayable()
+    {
+        return !played;
+    }
+
+    public void Reset()
+    {
+        played = false;
+    }
+
+    public bool IsPlaying()
+    {
+        return song.isPlaying;
+    }
+
+    public void StopSong()
+    {
+        if (!song.isPlaying) return;
+        song.Stop();
+    }
+}
 
 public class MusicPlayer : MonoBehaviour
 {
-    List<AudioSource> songs = new List<AudioSource>();
-    bool[] played;
-    int notPlayedRemaining;
-    int currentlyPlaying;
+    readonly private List<Song> songs = new();
+    private int playingIndex = -1;
+    private bool playing = false;
+    public bool skip = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        played = new bool[transform.childCount];
-        notPlayedRemaining = transform.childCount;
-        for (int i = 0; i < transform.childCount; i++)
+        foreach (Transform child in transform)
         {
-            songs.Add(transform.GetChild(i).GetComponent<AudioSource>());
-            played[i] = false;
+            songs.Add(new Song(child.GetComponent<AudioSource>()));
         }
-        var random = Random.Range(0, transform.childCount - 1);
-        songs[random].Play();
-        currentlyPlaying = random;
+
+        PlaySong();
     }
 
     void LateUpdate()
     {
-        if (!songs[currentlyPlaying].isPlaying)
+        if (skip) Skip();
+
+        if (playing) return;
+
+        PlaySong();
+    }
+
+    private void PlaySong()
+    {
+        List<Song> playableSongs = GetPlayableSongs();
+        int playIndex = Random.Range(0, playableSongs.Count - 1);
+        if (playIndex < 0) return;
+
+        Song song = playableSongs[playIndex];
+        int index = songs.IndexOf(song);
+        if (index == -1) return;
+
+        songs[index].PlaySong();
+        playingIndex = index;
+        playing = true;
+    }
+
+    private List<Song> GetPlayableSongs()
+    {
+        List<Song> playableSongs = new();
+        foreach (Song song in songs)
         {
-            played[currentlyPlaying] = true;
-            notPlayedRemaining--;
-            if(notPlayedRemaining == 0)
+            if (song.IsPlayable())
             {
-                for (int i = 0; i < played.Length; i++)
-                    played[i] = false;
-                notPlayedRemaining = played.Length;
-            }
-            while (true)
-            {
-                var random = Random.Range(0, transform.childCount - 1);
-                if(played[random] == false)
-                {
-                    songs[random].Play();
-                    currentlyPlaying = random;
-                }
+                playableSongs.Add(song);
             }
         }
+
+        if (playableSongs.Count > 0)
+        {
+            return playableSongs;
+        }
+
+        // Reset all songs as playable
+        foreach (Song song in songs)
+        {
+            if (!song.IsPlayable())
+            {
+                song.Reset();
+            }
+        }
+
+        return new(songs);
+    }
+
+    private void Skip()
+    {
+        skip = false;
+        playing = false;
+        songs[playingIndex].StopSong();
+        playingIndex = -1;
     }
 }
