d6554739dae60277d42b169c337e9938146fdbcf
Raikuman <40256283+Raikuman@users.noreply.github.com>
2023-04-29 22:53:18 -0700

    Implement concept

---
+++ Assets/UIIcons/UIHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    public void Clicked()
+    {
+        Debug.Log("Clicked");
+    }
+}
