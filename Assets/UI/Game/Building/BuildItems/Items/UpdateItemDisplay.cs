2f654b1db45899d160265913565560e1913e15ef
Nova <105680257+jsanfo@users.noreply.github.com>
2023-05-12 22:51:30 -0700

    Removed unused namespace in UpdateItemDisplay
    It seemed to be unused and was causing issues with building the game, so I deleted it.

--- Assets/UI/Game/Building/BuildItems/Items/UpdateItemDisplay.cs
+++ Assets/UI/Game/Building/BuildItems/Items/UpdateItemDisplay.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Progress;
 using UnityEngine.UI;
 
 public class UpdateItemDisplay : MonoBehaviour
